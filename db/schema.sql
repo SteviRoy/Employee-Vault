831c8edd10397abf9c2b40b787b2d50f082f7ad1
S.P. ROY <sprithi44@gmail.com>
2023-03-14 21:43:18 -0400

    vault_db and department table have been created

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS vault_db;
+
+CREATE DATABASE vault_db;
+
+USE vault_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
